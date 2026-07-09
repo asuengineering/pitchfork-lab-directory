71db7e6817abc053eb3a77a31fb76dd74a164bf4
Steve Ryan <steve.ryan@asu.edu>
2026-07-08 17:07:22 -0700

    Fix support for featued-thumbnails

--- includes/post-types.php
+++ includes/post-types.php
@@ -63,10 +63,7 @@ add_action( 'init', 'pfld_register_research_lab_post_type' );
  * limited thumbnail support to specific post types.
  */
 function pfld_enable_research_lab_featured_images() {
-	if ( ! current_theme_supports( 'post-thumbnails' ) ) {
-		add_theme_support( 'post-thumbnails' );
-	}
-
+	add_theme_support( 'post-thumbnails', array( 'research-lab' ) );
 	add_post_type_support( 'research-lab', 'thumbnail' );
 }
 add_action( 'after_setup_theme', 'pfld_enable_research_lab_featured_images', 20 );
